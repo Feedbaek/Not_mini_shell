44f02b4281cbbf85e96ea62e86bc43b295db7f3e
feedbaek <nicek789@naver.com>
2022-03-27 17:20:23 +0900

    pipex관련 함수 정리

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 16:09:11 by minskim2          #+#    #+#             */
-/*   Updated: 2022/03/24 17:08:35 by minskim2         ###   ########.fr       */
+/*   Updated: 2022/03/27 17:16:56 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,9 @@ typedef	struct	s_cmd
 }				t_cmd;
 
 int		minskim2_strncmp(const char *s1, const char *s2, size_t n);
+void	run_execve(t_cmd *cmd_arg, char *error_msg);
+void	wait_pid(t_cmd *cmd_arg);
+
 size_t	minskim2_strlen(const char *str);
 void	redirect_in(char *file, int input_fd);
 void	redirect_out(char *file, int output_fd);
@@ -42,8 +45,7 @@ int		path_finder(char **envp, t_cmd *x, char*split);
 char	*minskim2_strjoin(char const *s1, char const *s2);
 char	**minskim2_split(char const *s, char c);
 
-int		pipex(char **argv, char **envp);
-void	here_doc(t_cmd *cmd_arg, char *limiter);
+void	here_doc(t_cmd *cmd_arg);
 void	test_pipex(t_cmd *head);
 
 #endif
--- srcs/pipex/pipex.c
+++ srcs/pipex/pipex.c
@@ -6,82 +6,13 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 16:53:38 by minskim2          #+#    #+#             */
-/*   Updated: 2022/03/25 16:53:12 by minskim2         ###   ########.fr       */
+/*   Updated: 2022/03/27 17:17:38 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-//static void	cmd_init(const char *line, t_cmd *x, char **envp)
-//{
-//	char	**split;
-
-//	split = minskim2_split(line, ' ');
-//	if (!split)
-//	{
-//		printf("split error: %s\n", strerror(errno));
-//		exit(1);
-//	}
-//	if (ft_strncmp(split[1], "<<", 3) == 0)
-//	{
-//		x->limiter = split[2];
-//		split[2] = 0;
-//		free(split[1]);
-//		split[1] = 0;
-//	}
-//	if (!path_finder(envp, x, split[0]))
-//	{
-//		printf("init error: %s\n", strerror(errno));
-//		exit(1);
-//	}
-//	x->argv = split;
-//	x->envp = envp;
-//	x->next = 0;
-//}
-
-//int	check_length(char **split)
-//{
-//	int	cnt;
-
-//	cnt = 0;
-//	if (!split)
-//		exit(1);
-//	while(*split)
-//	{
-//		cnt++;
-//		split++;
-//	}
-//	return (cnt);
-//}
-
-//static void	set_pipex(char **argv, t_cmd **head, char **envp)
-//{
-//	int		cnt;
-//	int		i;
-//	t_cmd	*cmd_arg;
-//	t_cmd	*prev_cmd;
-
-//	cnt = check_length(argv);
-//	i = 0;
-//	while (i < cnt)
-//	{
-//		cmd_arg = malloc(sizeof(t_cmd));
-//		if (!cmd_arg)
-//			exit(1);
-//		if (i == 0)
-//		{
-//			*head = cmd_arg;
-//			prev_cmd = cmd_arg;
-//		}
-//		prev_cmd->next = cmd_arg;
-//		prev_cmd = cmd_arg;
-//		cmd_init(argv[i], cmd_arg, envp);
-//		cmd_arg->idx = i;
-//		i++;
-//	}
-//}
-
-void	run_execve(t_cmd *cmd_arg, int pipe_read, int pipe_write)
+static void	connect_stdin(t_cmd *cmd_arg, int pipe_read, int pipe_write)
 {
 	int fd;
 
@@ -96,9 +27,12 @@ void	run_execve(t_cmd *cmd_arg, int pipe_read, int pipe_write)
 		redirect_in(cmd_arg->redirect_in, STDIN_FILENO);
 	}
 	else {		// < 가  없으면
-		if (pipe_read != STDIN_FILENO)
-			dup2(pipe_read, STDIN_FILENO);
+		dup2(pipe_read, STDIN_FILENO);
 	}
+}
+
+static void	connect_stdout(t_cmd *cmd_arg, int pipe_read, int pipe_write)
+{
 	if (cmd_arg->redirect_out_add)	// >> 가  있으면
 	{
 		redirect_out_add(cmd_arg->redirect_out_add, STDOUT_FILENO);
@@ -115,16 +49,9 @@ void	run_execve(t_cmd *cmd_arg, int pipe_read, int pipe_write)
 	{
 		dup2(pipe_write, STDOUT_FILENO);
 	}
-	//dup2(pipe_read, STDIN_FILENO);
-	//dup2(pipe_write, STDOUT_FILENO);
-	if (execve(cmd_arg->cmd, cmd_arg->argv, cmd_arg->envp) == -1)
-	{
-		printf("run_execve error: %s\n", strerror(errno));
-		exit(1);
-	}
 }
 
-void	child_process(t_cmd *cmd_arg, int *pipe_a, int *pipe_b)
+static void	child_process(t_cmd *cmd_arg, int *pipe_a, int *pipe_b)
 {
 	int	read_fd;
 	int	write_fd;
@@ -140,52 +67,31 @@ void	child_process(t_cmd *cmd_arg, int *pipe_a, int *pipe_b)
 		write_fd = pipe_b[1];
 	}
 	if (cmd_arg->idx == 0)	// 처음 실행되는 명령어
-	{
 		read_fd = STDIN_FILENO;
-	}
 	if (cmd_arg->next == 0)	// 마지막으로 실행되는 명령어
-	{
 		write_fd = STDOUT_FILENO;
-	}
-	run_execve(cmd_arg, read_fd, write_fd);	// 명령어 실행
+	connect_stdin(cmd_arg, read_fd, write_fd);	// 표준입력 리다이렉트
+	connect_stdout(cmd_arg, read_fd, write_fd);	// 표준출력 리다이렉트
+	run_execve(cmd_arg, "run_execve error");	// 명령어 실행
 }
 
 void	disconnect_redirect(t_cmd *cmd_arg, int *pipe_a, int *pipe_b)
 {
-	//if (cmd_arg->limiter)
-	//	here_doc(cmd_arg, cmd_arg->limiter);
 	if (cmd_arg->idx % 2 == 0)	// 인덱스 짝수의 경우
 	{
 		pipe(pipe_a);
-		//if (cmd_arg->idx == 0)		// 처음 명령어인 경우
-		//{
-		//	if (cmd_arg->limiter)
-		//		here_doc(cmd_arg, cmd_arg->limiter);
-		//}
 		if (cmd_arg->idx != 0)	// 처음 명령어가 아닌경우 pipe_b의 쓰기를 닫아줘야함
-		{
-			//if (cmd_arg->limiter)
-			//	here_doc(cmd_arg, cmd_arg->limiter);
 			close(pipe_b[1]);
-		}
 		if (cmd_arg->next == 0)	// 마지막 명령어의 경우 pipe_a가 필요없음
 		{
-			//if (cmd_arg->limiter)
-			//	here_doc(cmd_arg, cmd_arg->limiter);
 			close(pipe_a[0]);
 			close(pipe_a[1]);
-			//if (cmd_arg->idx != 0)
-			//	close(pipe_b[1]);
 		}
 	}
 	else	// 인덱스 홀수인 경우 처음 명령어일 수는 없음
 	{
 		pipe(pipe_b);
 		close(pipe_a[1]);	// 안쓰는 pipe_a의 쓰기를 닫아줌
-		//if (cmd_arg->next != 0)		// 마지막 명령어가 아닌 경우
-		//{
-		//	close(pipe_a[1]);
-		//}
 		if (cmd_arg->next == 0)		// 마지막 명령어의 경우
 		{
 			close(pipe_b[0]);
@@ -201,30 +107,19 @@ void	test_pipex(t_cmd *head)
 	int		pipe_b[2];	// (마지막이 아닌)홀수인 경우에 쓰기, (처음이 아닌)짝수인 경우에 읽기
 	int		i;
 
+	//printf("test_pipex start\n");
+	here_doc(head);		// 명령어들을 실행하기 전에 한번 here_doc으로 임시파일들을 만들어줌
 	parser = head;
-	while (parser)		// 명령어들을 실행하기 전에 한번 here_doc으로 임시파일들을 만들어줌
-	{
-		if (parser->limiter)
-			here_doc(parser, parser->limiter);
-		parser = parser->next;
-	}
-	parser = head;
-	while (parser)
+	while (parser && parser->cmd)
 	{
 		disconnect_redirect(parser, pipe_a, pipe_b);	// 파이프를 리다이렉트 시켜줌
 		parser->pid = fork();
 		if (!parser->pid)
 			child_process(parser, pipe_a, pipe_b);	// 자식 프로세스에서 명령어 실행
-		//waitpid(parser->pid, &(parser->status), WNOWAIT);	// 비동기 형식으로 진행, 좀비 프로세스를 방지
 		parser = parser->next;
 	}
-	while (head)
-	{
-		waitpid(head->pid, &(head->status),0);	// 좀비 프로세스를 방지, 안료된 자식 프로세스의 상태 회수
-		if (head->limiter)
-			unlink(head->tmp);
-		head = head->next;
-	}
+	wait_pid(head);
+	//printf("test_pipex end\n");
 }
 
 
